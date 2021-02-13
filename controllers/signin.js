76a9e9b213afeb4a6e2993a24ce0969ee2c924f3
jcm005 <joseph.mattern@stonybrook.edu>
2021-02-12 22:19:50 -0500

    changes to controller/sigin

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,18 +1,18 @@
 
-const handleSignIn = (req, res, db, bcrypt) => {
+const handleSignIn = (db, bcrypt) => (req, res) => {
     const { email, password } = req.body;
     if (!email || !password) {
         return res.status(400).json('incorrect form submission');
     }
     db.select('email', 'hash').from('login')
-        .where('email', '=', req.body.email)
+        .where('email', '=', email)
         .then(response => {
             console.log(response[0]);
-            const isValid = bcrypt.compareSync(req.body.password, response[0].hash);
+            const isValid = bcrypt.compareSync(password, response[0].hash);
             if (isValid) {
 
                 return db.select('*').from('users')
-                    .where('email', '=', req.body.email)
+                    .where('email', '=', email)
                     .then(user => {
                         res.json(user[0])
                     })
